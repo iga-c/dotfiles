12fe4186ca0eb17f2f57173013b709c61e5dee69
Kazuya Igari <kazuya.igari@beaglescience.com>
2016-01-24 17:44:57 +0900

    シンボリックリンクを貼るシェルスクリプトを追加

---
+++ init.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
+mkdir ~/.vim
+ln -sf ~/dotfiles/.vim/colors ~/.vim/colors
+ln -sf ~/dotfiles/.vim/ftplugin ~/.vim/ftplugin
+ln -sf ~/dotfiles/.vim/filetype.vim ~/.vim/filetype.vim
